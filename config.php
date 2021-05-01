2358bad4bd789a52b3acc497b8534f26f50f8c1a
Jim Reevior <jim@hirozed.com>
2021-05-01 01:32:01 -0400

    We're going with jigsaw

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Boston Volunteer',
+    'description' => 'Boston Volunteers have been locally fighting for a cleaner, safer, and better Boston.',
+    'collections' => [],
+];
