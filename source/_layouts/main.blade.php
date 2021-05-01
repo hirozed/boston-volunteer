87ddd4fa9d14557d94dc58343ef2c3a6b76ecc45
ashleykolodziej <kolodz10@bu.edu>
2021-05-01 13:04:19 -0400

    Add Google web fonts (first pass)

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -7,6 +7,8 @@
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Domine&family=Fraunces&family=Newsreader&family=Petrona&display=swap" rel="stylesheet"> 
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
     <body class="text-gray-900 font-sans antialiased">
