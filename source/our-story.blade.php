1799ccef5f32245fa061d658ce95f923a0818896
ashleykolodziej <kolodz10@bu.edu>
2021-05-01 15:56:27 -0400

    Basic pages for all pages

--- source/our-story.blade.php
+++ source/our-story.blade.php
@@ -1,6 +1,6 @@
 @extends('_layouts.main')
 
-@section('title', 'Our Goals')
+@section('title', 'Our Story')
 
 @section('body')
 <section class="bv-content">
@@ -14,8 +14,8 @@
 			</video>
 		</div>
 		<section class="bv-hero-content">
-			<h1>Our Goals</h1>
-			<p>Copy for our goals.</p>
+			<h1>Our Story</h1>
+			<p>Copy for our story.</p>
 			<a href="#" class="bv-button bv-hero-button">Hello!</a>
 		</section>
 	</section>
