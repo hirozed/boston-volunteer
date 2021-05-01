7652f1c0d71842939621cf6e62628c969f0ac971
ashleykolodziej <kolodz10@bu.edu>
2021-05-01 14:14:41 -0400

    Navigation tie up!

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
-        <meta name="description" content="{{ $page->description }}">
-        <title>{{ $page->title }}</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Domine&family=Fraunces&family=Newsreader&family=Petrona&display=swap" rel="stylesheet"> 
-        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
-    </head>
-    <body class="text-gray-900 font-sans antialiased">
-        @yield('body')
-    </body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="canonical" href="{{ $page->getUrl() }}">
+		<meta name="description" content="{{ $page->description }}">
+		<title>{{ $page->title }}</title>
+		<link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Domine&family=Fraunces&family=Newsreader&family=Petrona&display=swap" rel="stylesheet"> 
+		<script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+	</head>
+	<body class="text-gray-900 font-sans antialiased">
+		<header>
+			<nav class="bv-nav" role="navigation">
+				<a href="/" id="bv-logo">
+					<img src="/assets/images/BV-new-01.svg" alt="Boston Volunteers">
+				</a>
+				<ul id="js-bv-nav" class="bv-nav-list">
+					<li class="bv-nav-item"><a class="bv-nav-link" href="/our-story">Our Story</a></li>
+					<li class="bv-nav-item"><a class="bv-nav-link" href="/our-goals">Our Goals</a></li>
+					<li class="bv-nav-item"><a class="bv-nav-link" href="/find-events">Find an Event</a></li>
+					<li class="bv-nav-item"><a class="bv-nav-link" href="/organize-events">Organize an Event</a></li>
+					<li class="bv-nav-item"><a class="bv-nav-link bv-nav-button" href="/get-involved">Get Involved</a></li>
+				</ul>
+			</nav>
+		</header>
+		@yield('body')
+	</body>
 </html>
