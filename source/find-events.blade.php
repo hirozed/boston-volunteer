b1c70c8800749e98f6c1282b03b31a840c953dd9
ashleykolodziej <kolodz10@bu.edu>
2021-05-01 23:20:46 -0400

    Update get involved

--- source/find-events.blade.php
+++ source/find-events.blade.php
@@ -14,7 +14,7 @@
 @section('hero-content')
 		<h1>Find Events</h1>
 		<p>Let us do the legwork. Pick an event you like, and we'll help you take your first steps in volunteering, no experience or special training required.</p>
-		<a href="#" class="bv-button bv-hero-button">Sign up on Meetup.com</a>
+		<a href="https://www.meetup.com/BostonVolunteer/about/" class="bv-button bv-hero-button">Sign up on Meetup.com</a>
 @endsection
 
 @section('body')
