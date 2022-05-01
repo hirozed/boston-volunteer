1647e1fcef5444b0e592122dcf0dd988d7a40cad
Ashley Kolodziej <kolodz10@bu.edu>
2022-05-01 19:23:57 -0400

    Update get-involved.blade.php

--- source/get-involved.blade.php
+++ source/get-involved.blade.php
@@ -23,25 +23,21 @@
 	<div class="bv-impact">
 		<aside class="bv-commit">
 			<h3>Volunteer</h3>
-			<p class="bv-commitment">Low commitment, minimal time</p>
 			<p>Get involved with your community without significant hassle or time commitments, or test the waters to see what volunteering is like. Pick any opportunity you're interested in that's convenient for you, and we'll handle the rest.</p>
 			<a href="https://www.meetup.com/BostonVolunteer/events/" class="bv-button">Find an event</a>
 		</aside>
 		<aside class="bv-commit">
 			<h3>Organize an Event</h3>
-			<p class="bv-commitment">Low commitment, medium time</p>
 			<p>Connect with the forces of Boston Volunteer by organizing your own volunteer event. Direct our community to the areas of greatest need and multiply your impact on your schedule. Just let us know how we can help.</p>
 			<a href="/organize-events" class="bv-button">Organize an event</a>
 		</aside>
 		<aside class="bv-commit">
 			<h3>Donate your skills</h3>
-			<p class="bv-commitment">Medium commitment, flexible time</p>
 			<p>Everyone has a superpower. What's yours? We need support of all sorts at the organizational level. Tell us how you think your skillset could help Boston Volunteer grow, and we'll help you grow your career in the process.</p>
 			<a href="mailto:josh@bostonvolunteer.org" class="bv-button">Contact Team Lead</a>
 		</aside>
 		<aside class="bv-commit">
 			<h3>Become an Organizer</h3>
-			<p class="bv-commitment">Medium commitment, structured time</p>
 			<p>If you are looking to grow in your leadership skills, or give back at the leadership level, we need people who can help grow, shape, and direct Boston Volunteer. We need voices of all levels of experience and backgrounds.</p>
 			<a href="mailto:josh@bostonvolunteer.org" class="bv-button">Contact Team Lead</a>
 		</aside>
